29b6df37d9e4094389de9a37e6116b039984af56
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2026-09-21 16:32:06 +0100

    feat: add the setup for tanstack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,36 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "MTikCode — Blog Demo",
-  description: "TanStack Query với Next.js",
+    title: "MTikCode — Blog Demo",
+    description: "TanStack Query với Next.js",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="vi"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html
+            lang="vi"
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
+            <body>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
